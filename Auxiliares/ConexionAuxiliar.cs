28d56a46ebfaed6e1bf6c609005f26b9c8c65f43
javierjolon <javierjolon.a@gmail.com>
2022-11-05 11:35:36 -0600

    first commit

---
+++ Auxiliares/ConexionAuxiliar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoDB2.Auxiliares
+{
+    public class ConexionAuxiliar
+    {
+       
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProyectoDB2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
